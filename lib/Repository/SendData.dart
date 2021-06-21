6b3ba25f8830988edfc4e3605f7958220eabf4bf
Dũng Nguyễn Đình <dungnd8594@gmail.com>
2021-06-21 13:46:29 +0700

    add null safety

--- lib/Repository/SendData.dart
+++ lib/Repository/SendData.dart
@@ -2,29 +2,32 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 
-sendMulticast(String ip , int port ,String message , Function (String)onDone){
-    RawDatagramSocket.bind(ip, port).then((RawDatagramSocket socket){
+sendMulticast(String ip, int port, String message, Function(String) onDone) {
+  RawDatagramSocket.bind(ip, port).then((RawDatagramSocket socket) {
     InternetAddress multicastAddress = new InternetAddress(ip);
     int multicastPort = port;
-    RawDatagramSocket.bind(InternetAddress.anyIPv4, 0 , reuseAddress: false , reusePort: false).then((RawDatagramSocket s) {
+    RawDatagramSocket.bind(InternetAddress.anyIPv4, 0,
+            reuseAddress: false, reusePort: false)
+        .then((RawDatagramSocket s) {
       s.send(message.codeUnits, multicastAddress, multicastPort);
-     
-      s.listen((RawSocketEvent e){
+
+      s.listen((RawSocketEvent e) {
         Datagram? d = s.receive();
-        if (d == null) return  ;
+        if (d == null) return;
         String messageRecived = new String.fromCharCodes(d.data);
-         onDone(messageRecived.trim()); 
+        onDone(messageRecived.trim());
       });
     });
   });
-  }
-  Future<String> httpPost(String url , String message)async{
+}
+
+Future<String> httpPost(String url, String message) async {
   Response response;
   Dio dio = Dio();
-  try{
-  response = await dio.post(url , data: message);
-  }catch (e){
-    return e.error.source.toString();
+  try {
+    response = await dio.post(url, data: message);
+  } catch (e) {
+    return e.toString();
   }
-   return response.data.toString();
+  return response.data.toString();
 }
